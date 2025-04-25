6546f2dc8fac59d9d7c2883f63922856e0892c99
jarlinson2007 <jarlinsonhenriquez@gmail.com>
2025-04-25 15:14:16 -0600

    final

--- src/labp1_jarlinson_salgado/Labp1_jarlinson_salgado.java
+++ src/labp1_jarlinson_salgado/Labp1_jarlinson_salgado.java
@@ -4,6 +4,8 @@
  */
 package labp1_jarlinson_salgado;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jarli
@@ -14,7 +16,48 @@ public class Labp1_jarlinson_salgado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int respuesta =-1;
+        Scanner read = new Scanner (System.in); 
+        
+        while(respuesta !=0){
+            System.out.println("Menu principal :");
+            System.out.println("opcion 1. mesaje");
+            System.out.println("opcion 2. calculadora");
+            System.out.println("opcion 3. edades ");
+            respuesta = read.nextInt();
+            
+            if (respuesta == 1){
+                System.out.println("hola soy yo");
+                // opcion 2
+            } else if (respuesta == 2){
+                System.out.println("ingrese el nuemro uno");
+                int num1 = read.nextInt();
+                System.out.println("ingese el segundo nuemro");
+                int num2 = read.nextInt();
+                int resultado = num1 * num2;
+                System.out.println("resultado:"+resultado);
+                
+                
+            }else if (respuesta == 3){
+                System.out.println("ingrese su edad :");
+                int edad =read.nextInt();
+                
+                if (respuesta  == 3){
+                System.out.println("ingrese su edad: ");
+                edad = read.nextInt();
+                
+                if(edad>=18){
+                    System.out.println("usted es mayor de edad");
+                    
+                }else{
+                    System.out.println("usted es menor de edad");
+                }
+            }
+                
+            }
+        }
     }
-    
+        // TODO code application logic here
 }
+    
+    
