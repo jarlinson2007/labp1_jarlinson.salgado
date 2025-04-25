295638e188e0face6240fd54c37fa9aaea3aef89
jarlinson2007 <jarlinsonhenriquez@gmail.com>
2025-04-25 14:05:12 -0600

    nuevo proyecto

---
+++ src/labp1_jarlinson_salgado/Labp1_jarlinson_salgado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labp1_jarlinson_salgado;
+
+/**
+ *
+ * @author jarli
+ */
+public class Labp1_jarlinson_salgado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
